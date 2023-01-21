0e9206c76ef1250912292c4ebedb3ca8b928c3cb
taxintt <nthouse14@gmail.com>
2023-01-21 15:18:28 +0900

    Implement PING command

--- app/server.go
+++ app/server.go
@@ -6,18 +6,32 @@ import (
 	"os"
 )
 
+func echoPong(conn net.Conn) {
+	defer conn.Close()
+	for {
+		_, err := conn.Write([]byte("+PONG\r\n"))
+		if err != nil {
+			return
+		}
+	}
+}
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	listener, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go echoPong(conn)
 	}
 }
