fc028b4e320dc8213d2347fd7f16b2859e45fd8f
taxintt <nthouse14@gmail.com>
2023-01-21 23:14:12 +0900

    refactoring redis server

--- app/server.go
+++ app/server.go
@@ -11,7 +11,8 @@ import (
 )
 
 var rexLeadingDigits = regexp.MustCompile(`\d+`)
-var isSinglePing = func(array []string) bool {
+
+func isSimplePing(array []string) bool {
 	rex := rexLeadingDigits.Copy()
 	argsLength, _ := strconv.Atoi(rex.FindString(array[0]))
 	return argsLength == 1
@@ -20,13 +21,16 @@ var isSinglePing = func(array []string) bool {
 func handleRequest(conn net.Conn) {
 	defer conn.Close()
 
-	var buffer []byte
+	// var buffer []byteではだめ
+	buffer := make([]byte, 50000)
+
 	if n, err := conn.Read(buffer); err != nil {
 		log.Println(n, "read error", err)
 	}
 
 	args := strings.Split(string(buffer), "\n")
-	if isSinglePing(args) {
+
+	if isSimplePing(args) {
 		if _, err := conn.Write([]byte("+PONG\r\n")); err != nil {
 			fmt.Println("Error writing data: ", err.Error())
 			os.Exit(1)
@@ -34,13 +38,16 @@ func handleRequest(conn net.Conn) {
 		return
 	}
 
-	for i := 4; i < len(args); i += 2 {
-		fmt.Printf("1) %#v\n", args[i])
-		if _, err := conn.Write([]byte("+" + args[i] + "\n")); err != nil {
-			fmt.Println("Error writing data: ", err.Error())
-			os.Exit(1)
-		}
-		continue
+	var resultArray []string
+	for i := 3; i < len(args)-1; i++ {
+		responseItem := args[i] + "\n"
+		resultArray = append(resultArray, responseItem)
+	}
+
+	allItems := strings.Join(resultArray, "")
+	if _, err := conn.Write([]byte(allItems)); err != nil {
+		fmt.Println("Error writing data: ", err.Error())
+		os.Exit(1)
 	}
 }
 
