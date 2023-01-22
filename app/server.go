f4eb1fff4ff0d0390b608d318bb9d3658f0e9339
taxintt <nthouse14@gmail.com>
2023-01-22 12:49:46 +0900

    Implement the logic switches ECHO and PING

--- app/server.go
+++ app/server.go
@@ -26,24 +26,29 @@ func handleRequest(conn net.Conn) {
 			panic(err)
 		}
 
-		// if _, err := conn.Write([]byte("+PONG\r\n")); err != nil {
-		// 	fmt.Println("Error writing data: ", err.Error())
-		// 	os.Exit(1)
-		// }
-
 		args := strings.Split(string(buffer), "\n")
+		command := strings.ToUpper(args[2])
 
-		var resultArray []string
-		for i := 3; i < len(args)-1; i++ {
-			responseItem := args[i] + "\n"
-			resultArray = append(resultArray, responseItem)
-		}
+		switch {
+		case strings.Contains(command, "PING"):
+			if _, err := conn.Write([]byte("+PONG\r\n")); err != nil {
+				fmt.Println("Error writing data: ", err.Error())
+				os.Exit(1)
+			}
+		case strings.Contains(command, "ECHO"):
+			var resultArray []string
+			for i := 3; i < len(args)-1; i++ {
+				responseItem := args[i] + "\n"
+				resultArray = append(resultArray, responseItem)
+			}
 
-		allItems := strings.Join(resultArray, "")
-		fmt.Println(allItems)
-		if _, err := conn.Write([]byte(allItems)); err != nil {
-			fmt.Println("Error writing data: ", err.Error())
-			os.Exit(1)
+			allItems := strings.Join(resultArray, "")
+			if _, err := conn.Write([]byte(allItems)); err != nil {
+				fmt.Println("Error writing data: ", err.Error())
+				os.Exit(1)
+			}
+		default:
+			fmt.Println("Error: this might be not redis command")
 		}
 	}
 }
