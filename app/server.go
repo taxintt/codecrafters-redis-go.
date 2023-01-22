3e6d129af975f0d070dad2a656936706a68852ce
taxintt <nthouse14@gmail.com>
2023-01-22 11:52:40 +0900

    Remove unused code

--- app/server.go
+++ app/server.go
@@ -5,17 +5,7 @@ import (
 	"io"
 	"net"
 	"os"
-	"regexp"
-	"strconv"
 )
-
-var rexLeadingDigits = regexp.MustCompile(`\d+`)
-
-func isSimplePing(array []string) bool {
-	rex := rexLeadingDigits.Copy()
-	argsLength, _ := strconv.Atoi(rex.FindString(array[0]))
-	return argsLength == 1
-}
 
 func handleRequest(conn net.Conn) {
 	defer conn.Close()
@@ -39,31 +29,6 @@ func handleRequest(conn net.Conn) {
 			fmt.Println("Error writing data: ", err.Error())
 			os.Exit(1)
 		}
-
-		// args := strings.Split(string(buffer), "\n")
-
-		// simple string case
-		// if isSimplePing(args) {
-		// 	if _, err := conn.Write([]byte("+PONG\r\n")); err != nil {
-		// 		fmt.Println("Error writing data: ", err.Error())
-		// 		os.Exit(1)
-		// 	}
-		// 	return
-		// }
-
-		// bulk string case
-		// fmt.Println("bulk string case")
-		// var resultArray []string
-		// for i := 3; i < len(args)-1; i++ {
-		// 	responseItem := args[i] + "\n"
-		// 	resultArray = append(resultArray, responseItem)
-		// }
-
-		// allItems := strings.Join(resultArray, "")
-		// if _, err := conn.Write([]byte(allItems)); err != nil {
-		// 	fmt.Println("Error writing data: ", err.Error())
-		// 	os.Exit(1)
-		// }
 	}
 }
 
