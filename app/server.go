ccaf86ddf71c79ffcb784a2511c01fe31b72540a
taxintt <nthouse14@gmail.com>
2023-02-22 10:55:16 +0900

    fix: refactoring

--- app/server.go
+++ app/server.go
@@ -6,55 +6,31 @@ import (
 	"net"
 	"os"
 	"strings"
+	"sync"
 )
 
-var memory = map[string]string{}
-
-func returnPong(conn net.Conn, args []string) {
-	if _, err := conn.Write([]byte("+PONG\r\n")); err != nil {
-		fmt.Println("Error writing data: ", err.Error())
-		os.Exit(1)
-	}
+type dataStore struct {
+	data map[string]string
+	mu   sync.RWMutex
 }
 
-func returnData(conn net.Conn, args []string) {
-	var resultArray []string
-	for i := 3; i < len(args)-1; i++ {
-		responseItem := args[i] + "\n"
-		resultArray = append(resultArray, responseItem)
-	}
-
-	allItems := strings.Join(resultArray, "")
-	if _, err := conn.Write([]byte(allItems)); err != nil {
-		fmt.Println("Error writing data: ", err.Error())
-		os.Exit(1)
-	}
+var store = dataStore{
+	mu:   sync.RWMutex{},
+	data: map[string]string{},
 }
 
-func setData(conn net.Conn, args []string) {
-	key := args[4]
-	value := args[6]
-	memory[key] = strings.TrimRight(value, "\r")
-	if _, err := conn.Write([]byte("+OK\r\n")); err != nil {
-		fmt.Println("Error writing data: ", err.Error())
-		os.Exit(1)
-	}
+func setData(key, value string) {
+	store.data[key] = value
 }
 
-func retrieveData(conn net.Conn, args []string) {
-	if value, ok := memory[args[4]]; ok {
-		RESPstring := "$" + fmt.Sprint(len(value)) + "\r\n" + value + "\r\n"
-		fmt.Println(RESPstring)
-		if _, err := conn.Write([]byte(RESPstring)); err != nil {
-			fmt.Println("Error writing data: ", err.Error())
-			os.Exit(1)
-		}
+func retrieveData(key string) string {
+	var respData string
+	if value, ok := store.data[key]; ok {
+		respData = fmt.Sprintf("$%d\r\n%s\r\n", len(value), value)
 	} else {
-		if _, err := conn.Write([]byte("+(nil)\r\n")); err != nil {
-			fmt.Println("Error writing data: ", err.Error())
-			os.Exit(1)
-		}
+		respData = "+(nil)\r\n"
 	}
+	return respData
 }
 
 func handleRequest(conn net.Conn) {
@@ -77,20 +53,31 @@ func handleRequest(conn net.Conn) {
 
 		// TODO
 		// does not consider the difference of RESP data types and there is no parse section for it
-		data := strings.Split(string(buffer), "\n")
-		command := strings.ToUpper(data[2])
+		respData := strings.Split(string(buffer), "\r\n")
+		command := strings.ToUpper(strings.TrimSpace(respData[2]))
 
-		switch {
-		case strings.Contains(command, "PING"):
-			returnPong(conn, data)
-		case strings.Contains(command, "ECHO"):
-			returnData(conn, data)
-		case strings.Contains(command, "SET"):
-			setData(conn, data)
-		case strings.Contains(command, "GET"):
-			retrieveData(conn, data)
+		var msg string
+		switch command {
+		case "PING":
+			msg = "+PONG\r\n"
+		case "ECHO":
+			msg = fmt.Sprintf("$%d\r\n%s\r\n", len(respData[4]), respData[4])
+		case "SET":
+			setData(respData[4], respData[6])
+			msg = "+OK\r\n"
+		case "GET":
+			msg = retrieveData(respData[4])
 		default:
-			conn.Write([]byte("-ERR unknown command '" + command + "'\r\n"))
+			msg = "-ERR unknown command '" + command + "'\r\n"
+		}
+
+		if _, err := conn.Write([]byte(msg)); err != nil {
+			if err == io.EOF {
+				fmt.Println("Info: io.EOF")
+				os.Exit(1)
+			}
+			fmt.Println("Error writing data: ", err.Error())
+			os.Exit(1)
 		}
 	}
 }
